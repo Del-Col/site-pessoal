6294bc83761777224bc439df0cd85988d679b5b5
Del-Col <henrydelcol94@gmail.com>
2021-06-08 18:34:36 -0300

    finalizei ex 02

---
+++ projetos/aprendendo javascript/ex02.js
@@ -0,0 +1,18 @@
+function subirtela() {
+    window.scrollTo ({
+        top:0,
+        behavior: "smooth"
+    });
+}
+
+function botao() {
+    if(window.scrollY === 0) {
+        // ocultar botão
+        document.querySelector('.scrollbutton').style.display = "none";
+    } else {
+        // mostrar botão
+        document.querySelector('.scrollbutton').style.display = "block";
+    }
+}
+
+window.addEventListener('scroll', botao);
